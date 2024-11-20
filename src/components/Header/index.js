ab56abd1f1cd47c0f0d97d4ad384a7ccdd46bd41
vivek <gurumath35@gmail.com>
2024-11-20 15:02:28 +0530

    Changes in Dash Board

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+
+
+const Header=()=>{
+    return(
+        <>
+        <h1>Header component</h1>
+        </>
+    )
+}
+export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+
+
+const Dashboard=()=>{
+    return(
+        <>
+        <h1>DashBoard Page</h1>
+        
+        </>
+    )
+}
+export default Dashboard;
